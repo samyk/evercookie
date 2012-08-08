98300f50bcad24be1e8f389224b7844d56eeddc5
Justin <justin@flipkey.com>
2012-08-08 10:37:51 -0400

    Remove dependency on apache for etags

--- evercookie_etag.php
+++ evercookie_etag.php
@@ -13,12 +13,32 @@
 if (!$_COOKIE["evercookie_etag"])
 {
 	// read our etag and pass back
-	if (function_exists('apache_request_headers')) {
-		$headers = apache_request_headers();
-		echo $headers['If-None-Match'];
-	} else {
-		header("HTTP/1.1 500 Internal Server Error");
-	}
+    if (!function_exists('apache_request_headers')) {
+
+        function apache_request_headers() {
+            $arh     = array();
+            $rx_http = '/\AHTTP_/';
+            foreach ($_SERVER as $key => $val) {
+                if (preg_match($rx_http, $key)) {
+                    $arh_key    = preg_replace($rx_http, '', $key);
+                    $rx_matches = array();
+                    // do some nasty string manipulations to restore the original letter case
+                    // this should work in most cases
+                    $rx_matches = explode('_', $arh_key);
+                    if (count($rx_matches) > 0 and strlen($arh_key) > 2) {
+                        foreach ($rx_matches as $ak_key => $ak_val)
+                            $rx_matches[$ak_key] = ucfirst($ak_val);
+                        $arh_key = implode('-', $rx_matches);
+                    }
+                    $arh[$arh_key] = $val;
+                }
+            }
+            return ($arh);
+        }
+    }
+
+    $headers = apache_request_headers();
+    echo $headers['If-None-Match'];
 
 	exit;
 }
